dd34af4e7a8526594805677cca3e011da7e38c04
Al Maslov <Al Maslov>
2018-09-28 16:56:33 +0300

    Add service configuration and base MVC

--- SimpleServer/Startup.cs
+++ SimpleServer/Startup.cs
@@ -5,16 +5,44 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration; 
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SimpleServer
 {
     public class Startup
     {
+        /// <summary>
+        /// Переменная окружения
+        /// </summary>
+        private readonly IHostingEnvironment _env; 
+
+        /// <summary>
+        /// Конфигирурем сервер
+        /// </summary>
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsetings.json", false, true)
+                .AddEnvironmentVariables(); 
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // сжатие            
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+            });
+
+            services.AddMvc(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +53,15 @@ namespace SimpleServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseResponseCompression();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
